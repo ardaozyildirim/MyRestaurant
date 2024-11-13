58d06da71906035a1c54ed97d095df7f5568309d
ardaozyildirim <ardaozyildirim2@gmail.com>
2024-11-13 15:02:09 +0300

    database synced with the docker

--- obj/Debug/net8.0/MyRestaurantApi.AssemblyInfo.cs
+++ obj/Debug/net8.0/MyRestaurantApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyRestaurantApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6d744517db55984325ab15df7ede5500092bd805")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ba9dad9f61e3cc0d7e6ca9af0e7cad8178283496")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyRestaurantApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyRestaurantApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
